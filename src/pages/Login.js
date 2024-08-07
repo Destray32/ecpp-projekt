62b7a7e14330d484decf27d4b0c5cee5f074d069
Destray32 <kubaj38@gmail.com>
2024-08-07 17:23:16 +0200

    password field

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,36 +7,42 @@ import "primereact/resources/themes/lara-light-cyan/theme.css";
 export default function LoginPage() {
 
     // listy z których wybierać mozna w dropdownach
-    const [avaibleLogins, setAvailableLogins] = useState(['test', 'test2']);
-    const [avaibleDbs, setAvailableDbs] = useState(['baza1', 'baza2']);
+    const [availableCompanies, setAvailableCompanies] = useState(['test', 'test2']);
+    const [availableLogins, setAvailableLogins] = useState(['login1', 'login2']);
 
-    // stany przechowywuajce login, haslo, baze danych
+    // stany przechowywuajce firme, login i hasło
+    const [firma, setFirma] = useState('');
     const [login, setLogin] = useState('');
-    const [db, setDb] = useState('');
     const [password, setPassword] = useState('');
 
-    useEffect(() => {
-        console.log(login, db, password);
-    }, [login, db, password]);
+    // useEffect(() => {
+    //     console.log(firma, login, password);
+    // }, [firma, login, password]);
 
     return (
-        <body className='bg-primary min-h-screen flex items-center justify-center'>
+        <main className='bg-primary min-h-screen flex items-center justify-center'>
             <div className='bg-white w-1/2 h-[20rem] rounded-lg drop-shadow-2xl'>
-                <form className='p-4 space-y-4 h-full'>
+                <form className='p-4 space-y-6 h-full'>
                     <div className='card flex flex-col drop-shadow-lg'>
-                        <Dropdown value={login} onChange={(e) => setLogin(e.value)} options={avaibleLogins} optionLabel="name"
+                        <Dropdown value={firma} onChange={(e) => setFirma(e.value)} options={availableCompanies} optionLabel="name"
+                            editable placeholder="Firma" autoComplete='off' className="w-full md:w-14rem p-4" />
+                    </div>
+                    <div className='card flex flex-col drop-shadow-lg'>
+                        <Dropdown value={login} onChange={(e) => setLogin(e.value)} options={availableLogins} optionLabel="name"
                             editable placeholder="Login" autoComplete='off' className="w-full md:w-14rem p-4" />
                     </div>
-                    <div className='card flex flex-col drop-shadow-lg'>
-                        <Dropdown value={db} onChange={(e) => setDb(e.value)} options={avaibleDbs} optionLabel="name"
-                            editable placeholder="Baza" autoComplete='off' className="w-full md:w-14rem p-4" />
+                    <div className='card flex flex-col drop-shadow-lg w-full'>
+                        <Password value={password} onChange={(e) => setPassword(e.target.value)} feedback={false} toggleMask placeholder="Hasło"
+                             inputClassName='w-full md:w-14rem h-[3rem] p-2'
+                             pt={{ iconField: { root: { className: 'w-full md:w-14rem ' } } }}
+                             />
                     </div>
-                    {/* <div className="card flex-col drop-shadow-lg">
-                        <Password value={password} onChange={(e) => setPassword(e.target.value)} autoComplete='off'
-                            inputClassName='w-[65rem]' feedback={false} />
-                    </div> */}
+                    <div className='flex justify-center'>
+                        <button className='bg-primary text-white p-2 rounded-md w-1/2'>Zaloguj</button>
+                    </div>
                 </form>
+                
             </div>
-        </body>
+        </main>
     )
 }
