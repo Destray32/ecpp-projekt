39f2cfc45857605792149b9a94739ef33246f6ec
Destray32 <kubaj38@gmail.com>
2024-08-09 11:32:30 +0200

    pracownik > logowanie, dane do logowanie

---
+++ src/data/PracLogowanieData.js
@@ -0,0 +1,45 @@
+
+const sampleData = [
+    {
+        name: "Jan",
+        surname: "Kowalski",
+        computer: "DEKSTOP-43261F",
+        version: "1.0.0",
+        date: "2021-09-01",
+        time: "12:00:00",
+    },
+    {
+        name: "Jan",
+        surname: "Kowalski",
+        computer: "DEKSTOP-43261F",
+        version: "1.0.0",
+        date: "2021-09-01",
+        time: "12:00:00",
+    },
+    {
+        name: "Jan",
+        surname: "Kowalski",
+        computer: "DEKSTOP-43261F",
+        version: "1.0.0",
+        date: "2021-09-01",
+        time: "12:00:00",
+    },
+    {
+        name: "Jan",
+        surname: "Kowalski",
+        computer: "DEKSTOP-43261F",
+        version: "1.0.0",
+        date: "2021-09-01",
+        time: "12:00:00",
+    },
+    {
+        name: "Jan",
+        surname: "Kowalski",
+        computer: "DEKSTOP-43261F",
+        version: "1.0.0",
+        date: "2021-09-01",
+        time: "12:00:00",
+    },
+];
+
+export default {sampleData};
--- src/pages/Pracownik/Logowanie.js
+++ src/pages/Pracownik/Logowanie.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import PracLogowanieData from "../../data/PracLogowanieData";
 
 export default function LogowaniePage() {
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
+    const [computer, setComputer] = useState("");
+    const [version, setVersion] = useState("");
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("");
+
+    // na przyszlosc pobieranie danych z API
+    useEffect(() => {
+        // fetch("https://api.example.com/data")
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        //         console.log(data);
+        //     });
+    }, []);
+
     return (
-        <div>
-            <h1>Logowanie</h1>
-        </div>
+        <main>
+            <div className="p-4">
+                <div className="outline outline-1 outline-gray-500">
+                    <table className="w-full">
+                        <thead className="bg-blue-700 text-white">
+                            <tr>
+                                <th className="border-r">Imię</th>
+                                <th className="border-r">Naziwsko</th>
+                                <th className="border-r">Komputer</th>
+                                <th className="border-r">Wersja</th>
+                                <th className="border-r">Data</th>
+                                <th className="border-r">Czas</th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-center">
+                            {PracLogowanieData.sampleData.map((item, i) => (
+                                <tr key={i} className="border-b even:bg-gray-200 odd:bg-gray-300">
+                                    <td className="border-r">{item.name}</td>
+                                    <td className="border-r">{item.surname}</td>
+                                    <td className="border-r">{item.computer}</td>
+                                    <td className="border-r">{item.version}</td>
+                                    <td className="border-r">{item.date}</td>
+                                    <td className="border-r">{item.time}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </main>
     )
 }
