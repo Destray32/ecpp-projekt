4907364b111ea1469cfc5a9c97b95ddde41bb131
SIZEEE71 <ma71bu.glowne@gmail.com>
2024-08-09 11:59:44 +0200

    Poprawki w czas > tydzien

--- src/pages/Czas/Tydzien.js
+++ src/pages/Czas/Tydzien.js
@@ -27,7 +27,7 @@ const sampleData = [
     }
 ];
 
-const WeekPicker = () => {
+export default function TydzienPage() {
     const [selectedWeek, setSelectedWeek] = useState('');
     const [weekRange, setWeekRange] = useState({ start: '', end: '' });
     const [selectedItems, setSelectedItems] = useState([]);
@@ -83,53 +83,54 @@ const WeekPicker = () => {
     return (
         <div>
             <AmberBox>
-                <div className="flex flex-row items-center">
+                <div className="flex flex-col items-center space-y-8 p-4 w-full">
                     <div className="flex flex-row items-center">
-                        <p className="mr-2">Wybierz tydzień:</p>
-                        <input type="week" onChange={handleWeekChange} />
-                        <div className="ml-4">
-                            <p>
-                                {weekRange.start && weekRange.end ? ` ${weekRange.start} - ${weekRange.end}` : 'Wybierz tydzień, aby zobaczyć przedział dni'}
-                            </p>
+                        <div className="flex flex-row items-center">
+                            <p className="mr-2">Wybierz tydzień:</p>
+                            <input type="week" onChange={handleWeekChange} />
+                            <div className="ml-4">
+                                <p>
+                                    {weekRange.start && weekRange.end ? ` ${weekRange.start} - ${weekRange.end}` : 'Wybierz tydzień, aby zobaczyć przedział dni'}
+                                </p>
+                            </div>
+                            <Button label="Otwórz tydzień" className="p-button-outlined border-2 p-1 bg-white pr-2 pl-2 ml-6" />
+                            <Button label="Zamknij tydzień" className="p-button-outlined border-2 p-1 bg-white pr-2 pl-2 ml-6" />
+                            <Button label="Drukuj" className="p-button-outlined border-2 p-1 bg-white pr-2 pl-2 ml-6" />
                         </div>
-                        <Button label="Otwórz tydzień" className="p-button-outlined border-2 p-1 bg-white pr-2 pl-2 ml-6" />
-                        <Button label="Zamknij tydzień" className="p-button-outlined border-2 p-1 bg-white pr-2 pl-2 ml-6" />
-                        <Button label="Drukuj" className="p-button-outlined border-2 p-1 bg-white pr-2 pl-2 ml-6" />
                     </div>
+                    
                 </div>
             </AmberBox>
             <div className="w-full md:w-auto bg-gray-300 h-full m-2 outline outline-1 outline-gray-500">
-                <table className="w-full">
-                    <thead className="bg-blue-700 text-white">
-                        <tr>
-                            <th></th>
-                            <th className="border-r">Imię i nazwisko</th>
-                            <th className="border-r">Grupa urlopowa</th>
-                            <th className="border-r">Status</th>
-                            <th className="border-r">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody className="text-center">
-                        {sampleData.map((item) => (
-                            <tr key={item.id} className="border-b even:bg-gray-200 odd:bg-gray-300">
-                                <td className="border-r">
-                                    <Checkbox 
-                                        inputId={`cb-${item.id}`}
-                                        checked={selectedItems.includes(item.id)}
-                                        onChange={() => handleCheckboxChange(item.id)}
-                                    />
-                                </td>
-                                <td className="border-r">{item.name}</td>
-                                <td className="border-r">{item.grupaurlopowa}</td>
-                                <td className="border-r text-green-600">{item.status1}</td>
-                                <td className="border-r text-red-600">{item.status2}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                        <table className="w-full">
+                            <thead className="bg-blue-700 text-white">
+                                <tr>
+                                    <th></th>
+                                    <th className="border-r">Imię i nazwisko</th>
+                                    <th className="border-r">Grupa urlopowa</th>
+                                    <th className="border-r">Status</th>
+                                    <th className="border-r">Status</th>
+                                </tr>
+                            </thead>
+                            <tbody className="text-center">
+                                {sampleData.map((item) => (
+                                    <tr key={item.id} className="border-b even:bg-gray-200 odd:bg-gray-300">
+                                        <td className="border-r">
+                                            <Checkbox 
+                                                inputId={`cb-${item.id}`}
+                                                checked={selectedItems.includes(item.id)}
+                                                onChange={() => handleCheckboxChange(item.id)}
+                                            />
+                                        </td>
+                                        <td className="border-r">{item.name}</td>
+                                        <td className="border-r">{item.grupaurlopowa}</td>
+                                        <td className="border-r text-green-600">{item.status1}</td>
+                                        <td className="border-r text-red-600">{item.status2}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
         </div>
     );
-};
-
-export default WeekPicker;
+}
