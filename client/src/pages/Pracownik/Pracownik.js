81f809296d378f120516a684675eeffbfe5a5e65
Tomasz <141136111+TWSlark@users.noreply.github.com>
2024-08-17 13:50:15 +0200

    Zrobienie componentu EditableCells

--- client/src/pages/Pracownik/Pracownik.js
+++ client/src/pages/Pracownik/Pracownik.js
@@ -9,67 +9,11 @@ import axios from 'axios';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
 
+import EditableCell from '../../Components/EditableCells';
+
 import { font } from "../../fonts/OpenSans-Regular-normal";
 
 const { Option } = Select;
-
-const EditableCell = ({
-  title,
-  editable,
-  children,
-  selectOptions = [],
-  record,
-  onSave,
-  ...restProps
-}) => {
-  const [form] = Form.useForm();
-  const inputRef = useRef(null);
-
-  const handleChange = value => {
-    form.setFieldsValue({ [title]: value });
-  };
-
-  const handleSave = async () => {
-    try {
-      const updatedValue = form.getFieldValue(title);
-      await onSave(record.id, title, updatedValue);
-    } catch (error) {
-      console.error('Save failed:', error);
-    }
-  };
-
-  useEffect(() => {
-    form.setFieldsValue({ [title]: children });
-  }, [children, title, form]);
-
-  return (
-    <td {...restProps}>
-      {editable ? (
-        <Form form={form} initialValues={{ [title]: children }}>
-          <Form.Item
-            style={{ margin: 0 }}
-            name={title}
-            rules={[{ required: true, message: `${title} is required.` }]}
-          >
-            <Select
-              ref={inputRef}
-              onChange={handleChange}
-              onBlur={handleSave}
-            >
-              {selectOptions.map(option => (
-                <Option key={option.id} value={option.id}>
-                  {option.name}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
-        </Form>
-      ) : (
-        children
-      )}
-    </td>
-  );
-};
 
 export default function PracownikPage() {
   const [tableData, setTableData] = useState([]);
