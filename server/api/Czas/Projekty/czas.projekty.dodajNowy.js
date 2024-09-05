ac83d1d3a96e5af11433067f212f7cf4b69dc0e3
Tomasz <141136111+TWSlark@users.noreply.github.com>
2024-09-05 13:23:22 +0200

    edytowanie projektów, poprawa dodawania

--- server/api/Czas/Projekty/czas.projekty.dodajNowy.js
+++ server/api/Czas/Projekty/czas.projekty.dodajNowy.js
@@ -2,9 +2,8 @@
 function DodajNowyProjekt(req, res, db) {
     const {firma, zleceniodawca, nazwa, ulica, miejscowosc, kodPocztowy, kraj} = req.body;
     const sql = `INSERT INTO projekty (Firma, Zleceniodawca, NazwaKod_Projektu, Ulica, Miejscowosc, Kod_Pocztowy, Kraj)
-     VALUES ('${firma}', '${zleceniodawca}', '${nazwa}', '${ulica}', '${miejscowosc}', '${kodPocztowy}', '${kraj}')`;
+     VALUES ('${firma}', '${zleceniodawca}', '${nazwa.name}', '${ulica}', '${miejscowosc}', '${kodPocztowy}', '${kraj}')`;
     
-
     db.query(sql, (err, result) => {
     if (err) {
         res.status(400).send('Brak wymaganych danych');
