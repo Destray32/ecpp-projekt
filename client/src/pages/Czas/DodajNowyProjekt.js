400f043dfcd3934b3f1a3834bc7034116c1c03b0
Destray32 <kubaj38@gmail.com>
2024-09-19 14:50:52 +0200

    walidowanie inputów w dodawaniu projektu po stronie frontend, klasyczne zepsucie formularzy

--- client/src/pages/Czas/DodajNowyProjekt.js
+++ client/src/pages/Czas/DodajNowyProjekt.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AmberBox from "../../Components/AmberBox";
-import { Input, Select, Button, AutoComplete } from "antd";
+import { Input, Select, Button, AutoComplete, Form } from "antd";
 import Axios from "axios";
+
+import { universalFieldValidation, requiredField } from "../../utils/validationRules";
 
 const { Option } = Select;
 
@@ -10,22 +12,14 @@ export default function DodajNowyProjektPage() {
     const [availableGroups, setAvailableGroups] = useState([]);
     const [availableProjects, setAvailableProjects] = useState([]);
 
-    const [form, setForm] = useState({
-        firma: '1',
-        zleceniodawca: '',
-        nazwa: '',
-        ulica: '',
-        miejscowosc: '',
-        kodPocztowy: '',
-        kraj: ''
-    });
+    const [form] = Form.useForm();
 
     const fetchGroups = () => {
         Axios.get("http://localhost:5000/api/grupy", { withCredentials: true })
             .then((response) => {
-                const transformedData = response.data.grupy.map(grupy => ({
+                const transformedData = response.data.grupy.map((grupy) => ({
                     name: grupy.Zleceniodawca,
-                    value: grupy.id
+                    value: grupy.id,
                 }));
                 setAvailableGroups(transformedData);
             })
@@ -37,9 +31,9 @@ export default function DodajNowyProjektPage() {
     const fetchProjects = () => {
         Axios.get("http://localhost:5000/api/czas/projekty", { withCredentials: true })
             .then((response) => {
-                const transformedDataProjects = response.data.projekty.map(projekty => ({
+                const transformedDataProjects = response.data.projekty.map((projekty) => ({
                     name: projekty.NazwaKod_Projektu,
-                    value: projekty.idProjekty
+                    value: projekty.idProjekty,
                 }));
                 setAvailableProjects(transformedDataProjects);
             })
@@ -53,29 +47,14 @@ export default function DodajNowyProjektPage() {
         fetchProjects();
     }, []);
 
-    const handleSave = () => {
-        console.log('Form:', form);
-        Axios.post('http://localhost:5000/api/czas/projekty', form, { withCredentials: true })
-            .then(res => {
+    const handleSave = (values) => {
+        Axios.post('http://localhost:5000/api/czas/projekty', values, { withCredentials: true })
+            .then((res) => {
                 window.location.href = '/home/projekty';
             })
-            .catch(err => {
+            .catch((err) => {
                 console.error(err);
             });
-    };
-
-    const handleChange = (value, field) => {
-        setForm(prevState => ({
-            ...prevState,
-            [field]: value
-        }));
-    };
-
-    const handleGroupChange = (value) => {
-        setForm(prevState => ({
-            ...prevState,
-            zleceniodawca: value
-        }));
     };
 
     return (
@@ -85,82 +64,96 @@ export default function DodajNowyProjektPage() {
             </div>
             <AmberBox>
                 <div className="flex flex-col items-center space-y-8 p-4 w-full">
-
-                    <div className="w-full md:w-6/12 lg:w-4/12">
-                        <label htmlFor="firma">Firma</label>
-                        <Select
-                            onChange={(value) => handleChange(value, 'firma')}
-                            className="w-full"
-                            value={form.firma}
-                            showSearch
+                    <Form
+                        form={form}
+                        onFinish={handleSave}
+                        initialValues={{ firma: '1' }}
+                        labelCol={{ span: 16 }}
+                        wrapperCol={{ span: 32 }}
+                        layout="vertical"
+                        className="w-full max-w-[600px] items-center justify-center"
+                    >
+                        <Form.Item
+                            name="firma"
+                            label="Firma"
+                            rules={[requiredField("Firma is required")]}
                         >
-                            <Option key="1" value="1">
-                                PC Husbyggen
-                            </Option>
-                        </Select>
-                    </div>
+                            <Select>
+                                <Option key="1" value="1">PC Husbyggen</Option>
+                            </Select>
+                        </Form.Item>
 
-                    <div className="w-full md:w-6/12 lg:w-4/12">
-                        <label htmlFor="zleceniodawca">Zleceniodawca</label>
-                        <Select
-                            onChange={handleGroupChange}
-                            className="w-full"
-                            value={form.zleceniodawca}
-                            showSearch
-                            allowClear
+                        <Form.Item
+                            name="zleceniodawca"
+                            label="Zleceniodawca"
+                            rules={[requiredField("Zleceniodawca is required")]}
                         >
-                            {availableGroups.map((group) => (
-                                <Option key={group.value} value={group.value}>
-                                    {group.name}
-                                </Option>
-                            ))}
-                        </Select>
-                    </div>
+                            <Select showSearch allowClear>
+                                {availableGroups.map((group) => (
+                                    <Option key={group.value} value={group.value}>
+                                        {group.name}
+                                    </Option>
+                                ))}
+                            </Select>
+                        </Form.Item>
 
-                    <div className="w-full md:w-6/12 lg:w-4/12">
-                        <label htmlFor="nazwa">Nazwa/Kod Projektu</label>
-                        <AutoComplete
-                            onChange={(value) => handleChange(value, 'nazwa')}
-                            className="w-full"
-                            value={form.nazwa}
-                            options={availableProjects.map(project => ({
-                                value: project.value,
-                                label: project.name
-                            }))}
-                            placeholder="Wybierz projekt"
-                            filterOption={(inputValue, option) =>
-                                option.label.toLowerCase().includes(inputValue.toLowerCase())
-                            }
-                            allowClear
-                        />
-                    </div>
+                        <Form.Item
+                            name="nazwa"
+                            label="Nazwa/Kod Projektu"
+                            rules={[requiredField("Nazwa is required")]}
+                        >
+                            <AutoComplete
+                                options={availableProjects.map((project) => ({
+                                    value: project.value,
+                                    label: project.name,
+                                }))}
+                                placeholder="Wybierz projekt"
+                                filterOption={(inputValue, option) =>
+                                    option.label.toLowerCase().includes(inputValue.toLowerCase())
+                                }
+                                allowClear
+                            />
+                        </Form.Item>
 
-                    <div className="w-full md:w-6/12 lg:w-4/12">
-                        <label htmlFor="ulica">Ulica</label>
-                        <Input onChange={(e) => handleChange(e.target.value, 'ulica')} value={form.ulica} />
-                    </div>
+                        <Form.Item
+                            name="ulica"
+                            label="Ulica"
+                            rules={[universalFieldValidation("ulica")]}
+                        >
+                            <Input />
+                        </Form.Item>
 
-                    <div className="w-full md:w-6/12 lg:w-4/12">
-                        <label htmlFor="miejscowosc">Miejscowość</label>
-                        <Input onChange={(e) => handleChange(e.target.value, 'miejscowosc')} value={form.miejscowosc} />
-                    </div>
+                        <Form.Item
+                            name="miejscowosc"
+                            label="Miejscowość"
+                            rules={[universalFieldValidation("miejscowosc")]}
+                        >
+                            <Input />
+                        </Form.Item>
 
-                    <div className="w-full md:w-6/12 lg:w-4/12">
-                        <label htmlFor="kodPocztowy">Kod pocztowy</label>
-                        <Input onChange={(e) => handleChange(e.target.value, 'kodPocztowy')} value={form.kodPocztowy} />
-                    </div>
+                        <Form.Item
+                            name="kodPocztowy"
+                            label="Kod pocztowy"
+                            rules={[requiredField("Kod pocztowy is required")]}
+                        >
+                            <Input />
+                        </Form.Item>
 
-                    <div className="w-full md:w-6/12 lg:w-4/12">
-                        <label htmlFor="kraj">Kraj</label>
-                        <Input onChange={(e) => handleChange(e.target.value, 'kraj')} value={form.kraj} />
-                    </div>
+                        <Form.Item
+                            name="kraj"
+                            label="Kraj"
+                            rules={[universalFieldValidation("kraj")]}
+                        >
+                            <Input />
+                        </Form.Item>
 
-                    <div className="flex space-x-4 mt-8">
-                        <Button type="primary" onClick={handleSave}>Zapisz</Button>
-                        <Link to="/home/projekty">
-                            <Button>Anuluj</Button>
-                        </Link>
-                    </div>
+                        <div className="flex space-x-4 mt-8 justify-center">
+                            <Button type="primary" htmlType="submit">Zapisz</Button>
+                            <Link to="/home/projekty">
+                                <Button>Anuluj</Button>
+                            </Link>
+                        </div>
+                    </Form>
                 </div>
             </AmberBox>
         </div>
