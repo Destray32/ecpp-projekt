600dd68798f49fe0037a1d82b50f59b40065bc3f
SIZEEE71 <ma71bu.glowne@gmail.com>
2024-11-21 16:07:25 +0100

    urlopy poprawione lekko

--- client/src/Components/VacationPlanner copy.js
+++ client/src/Components/VacationPlanner copy.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { format, addDays, getISOWeek, startOfWeek, endOfWeek, isWithinInterval, parseISO, setISOWeek } from 'date-fns';
+import { format, addDays, getISOWeek, parseISO, setISOWeek, startOfWeek, isWithinInterval } from 'date-fns';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
-
-
 
 const VacationPlanner = () => {
     const [vacationData, setVacationData] = useState([]);
@@ -11,7 +9,6 @@ const VacationPlanner = () => {
     const [endDate, setEndDate] = useState(new Date());
     const plannerRef = useRef();
 
-    // pobiera i zwraca rok z localStorage lub aktualny rok jeśli nie ma
     const getYearLocalStorage = () => {
         const storedSelectedWeekAndYear = localStorage.getItem('selectedWeekAndYear');
         if (storedSelectedWeekAndYear) {
@@ -25,71 +22,83 @@ const VacationPlanner = () => {
         const storedSelectedWeekAndYear = localStorage.getItem('selectedWeekAndYear');
         if (storedSelectedWeekAndYear) {
             const [week, year] = JSON.parse(storedSelectedWeekAndYear);
-
-            // Calculate the start date based on the selected week and year
             const calculatedStartDate = setISOWeek(new Date(year, 0, 1), parseInt(week, 10));
-
-            // Set the start date and end date
             setStartDate(calculatedStartDate);
-            setEndDate(new Date(year, 11, 31)); // End date for the entire year
+            setEndDate(new Date(year, 11, 31));
         }
 
         const storedVacationData = localStorage.getItem('vacationData');
         if (storedVacationData) {
             setVacationData(JSON.parse(storedVacationData));
         }
-        const timeoutId = setTimeout(() => {
-            downloadPDF();
-        }, 1000);
-
-        return () => clearTimeout(timeoutId);
     }, []);
 
-    const getWeekDates = (date) => {
-        const start = startOfWeek(date, { weekStartsOn: 1 });
-        const end = endOfWeek(date, { weekStartsOn: 1 });
-        return { start, end };
-    };
-
     const renderHeader = () => {
-        let currentDate = startDate;
         const headers = [];
+        let currentDate = startDate;
+        let weekNumber = getISOWeek(currentDate);
+
         while (currentDate <= endDate) {
-            const { start, end } = getWeekDates(currentDate);
+            const weekStartDate = startOfWeek(currentDate, { weekStartsOn: 1 });
+
             headers.push(
-                <th key={currentDate.toISOString()} className="border text-xs" colSpan={7}>
-                    <div>{getISOWeek(currentDate)}</div>
+                <th
+                    key={weekStartDate.toISOString()}
+                    className="border border-gray-400 text-xl w-[20px] text-center"
+                    colSpan="7"
+                >
+                    {`${weekNumber}`}
                 </th>
             );
-            currentDate = addDays(currentDate, 7);
+
+            // Move to the next week
+            currentDate = addDays(weekStartDate, 7);
+            weekNumber = getISOWeek(currentDate);
         }
         return headers;
     };
 
-    const renderDayHeaders = () => {
-        return ['P', 'W', 'S', 'C', 'P', 'S', 'N'].map((day, index) => (
-            <th key={index} className="border text-xs w-[1px]">
-                {day}
-            </th>
-        ));
+    const downloadPDF = () => {
+        const input = plannerRef.current;
+        
+        html2canvas(input, {
+            scale: 2,
+            useCORS: true,
+            logging: false,
+            allowTaint: true
+        }).then((canvas) => {
+            const imgData = canvas.toDataURL('image/png');
+            
+            const pdf = new jsPDF('l', 'px', 'a4', true);
+            const imgWidth = pdf.internal.pageSize.getWidth() - 20; 
+            const imgHeight = (canvas.height * imgWidth) / canvas.width;
+    
+            pdf.addImage(imgData, 'PNG', 10, 10, imgWidth, imgHeight, undefined, 'FAST'); 
+            pdf.save('Urlopy.pdf');
+        });
     };
+    
 
     const renderVacationRow = (employee) => {
         let currentDate = startDate;
         const cells = [];
+        let weekNumber = getISOWeek(currentDate);
 
         while (currentDate <= endDate) {
+            const weekStartDate = startOfWeek(currentDate, { weekStartsOn: 1 });
+
             for (let i = 0; i < 7; i++) {
                 let bgColor = '';
+                const isSunday = format(currentDate, 'i') === '7'; 
 
-                employee.vacations.forEach(vacation => {
+                employee.vacations.forEach((vacation) => {
                     const from = parseISO(vacation.from);
                     const to = parseISO(vacation.to);
 
                     if (isWithinInterval(currentDate, { start: from, end: to })) {
                         switch (vacation.status) {
                             case 'Zatwierdzone':
-                                bgColor = 'bg-green-200';
+                                bgColor = 'bg-green-300';
                                 break;
                             case 'Do zatwierdzenia':
                                 bgColor = 'bg-yellow-200';
@@ -103,43 +112,21 @@ const VacationPlanner = () => {
                     }
                 });
 
+                const cellStyle = `${bgColor} ${isSunday ? 'bg-red-100' : ''}`;
+
                 cells.push(
                     <td
                         key={currentDate.toISOString()}
-                        className={`border ${bgColor} ${i === 6 ? 'bg-red-100' : ''}`}
-                    >
-                    </td>
+                        className={`border border-gray-400 overflow-hidden truncate ${cellStyle}`}
+                    ></td>
                 );
 
                 currentDate = addDays(currentDate, 1);
             }
+
+            currentDate = addDays(weekStartDate, 7);
         }
         return cells;
-    };
-
-    const downloadPDF = () => {
-        const input = plannerRef.current;
-        html2canvas(input).then(canvas => {
-            const imgData = canvas.toDataURL('image/png');
-            const pdf = new jsPDF('l', 'mm', 'a4');
-            const imgWidth = 297; // A4 width 
-            const pageHeight = 210; // A4 height
-            const imgHeight = (canvas.height * imgWidth) / canvas.width;
-            let heightLeft = imgHeight;
-            let position = 0;
-
-            pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-            heightLeft -= pageHeight;
-
-            while (heightLeft >= 0) {
-                position = heightLeft - imgHeight;
-                pdf.addPage();
-                pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-                heightLeft -= pageHeight;
-            }
-
-            pdf.save('VacationPlanner.pdf');
-        });
     };
 
     return (
@@ -149,20 +136,17 @@ const VacationPlanner = () => {
                 Pobierz PDF
             </button>
             <div ref={plannerRef} className="overflow-x-auto">
-                <table className="w-full border-collapse text-center">
+                <table className="w-full border-collapse text-center border-gray-400 table-fixed">
                     <thead>
                         <tr>
-                            <th className="border p-2 w-1/12">Firma</th>
-                            <th className="border p-2 w-1/12">Imie i Nazwisko</th>
+                            <th className="border border-gray-400 p-2 truncate w-[55px]">Imię i Nazwisko</th>
                             {renderHeader()}
                         </tr>
-
                     </thead>
                     <tbody>
-                        {vacationData.map((employee, index) => (
+                        {vacationData.map((employee) => (
                             <tr key={employee.id}>
-                                <td className="border p-2 w-1/12">PC Husbyggen</td>
-                                <td className="border p-2 w-1/12">{employee.name}</td>
+                                <td className="border border-gray-400 p-2 w-[55px] truncate">{employee.name}</td>
                                 {renderVacationRow(employee)}
                             </tr>
                         ))}
