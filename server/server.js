6c75e0848fe3f08567aa85d777a80efe448d4cf8
SIZEEE71 <ma71bu.glowne@gmail.com>
2025-08-26 18:15:39 +0200

    Brakło mi server.js

--- server/server.js
+++ server/server.js
@@ -585,19 +585,19 @@ app.get('/api/firmy', (req, res) => {
     PobierzDostepneFirmy(req, res, pool);
 });
 
-// const options = {
-//     cert: fs.readFileSync('/home/opc/ECPP/certs/fullchain.pem'),
-//     key: fs.readFileSync('/home/opc/ECPP/certs/privkey.pem')
-// };
+const options = {
+    cert: fs.readFileSync('/home/opc/ECPP/certs/fullchain.pem'),
+    key: fs.readFileSync('/home/opc/ECPP/certs/privkey.pem')
+};
 
-// https.createServer(options, app).listen(5000, () => {
-//     console.log('Server running on https://qubis.pl:5000');
-// });
-
-
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+https.createServer(options, app).listen(5000, () => {
+    console.log('Server running on https://qubis.pl:5000');
 });
+
+
+// app.listen(port, () => {
+//     console.log(`Server running on http://localhost:${port}`);
+// });
 
 
 
