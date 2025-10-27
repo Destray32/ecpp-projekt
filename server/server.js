01c7ab4e303d9c88ee4a741dfb6968547e25f4d6
Oracle Public Cloud User <opc@minecraft-server.subnet10181740.vcn10181740.oraclevcn.com>
2025-10-27 12:20:22 +0100

    Console loge aby w koncu zobaczyc gdzie jest problem z Corsem

--- server/server.js
+++ server/server.js
@@ -53,6 +53,7 @@ app.use(cors({
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
+            console.log('Blocked origin:', origin);
             callback(new Error('Not allowed by CORS'));
         }
     },
